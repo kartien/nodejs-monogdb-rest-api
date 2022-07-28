dd7a02e4a054f86f4a35cfc78016b477b444d69d
kartien <kevinramon523@gmail.com>
2022-07-28 14:52:53 -0500

    Add method delete and put

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -9,6 +9,7 @@ export const createTask = async (req, res) => {
   const newTask = new Task({
     title: req.body.title,
     description: req.body.description,
+    done: req.body.done ? req.body.done : false,
   });
   const taskSave = await newTask.save();
   res.json(taskSave);
@@ -18,3 +19,20 @@ export const findOneTask = async (req, res) => {
   const task = await Task.findById(req.params.id);
   res.json(task);
 };
+
+export const deleteTask = async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({
+    message: `Task were delete successfully`,
+  });
+};
+
+export const findAllDoneTasks = async (req, res) => {
+  const task = await Task.find({ done: true });
+  res.json(task);
+};
+
+export const updateTask = async (req, res) =>  {
+  await Task.findByIdAndUpdate(req.params.id, req.body)
+  res.json({message: 'Task updated successfully'});
+}
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,13 @@
 import { Router } from "express";
 
-import { findAllTasks, createTask, findOneTask } from "../controllers/task.controller";
+import {
+  findAllTasks,
+  createTask,
+  findOneTask,
+  deleteTask,
+  findAllDoneTasks,
+  updateTask,
+} from "../controllers/task.controller";
 
 const router = Router();
 
@@ -8,8 +15,12 @@ router.post("/task", createTask);
 
 router.get("/task", findAllTasks);
 
+router.get("/task/done", findAllDoneTasks);
+
 router.get("/task/:id", findOneTask);
 
+router.delete("/task/:id", deleteTask);
 
+router.put("/task/:id", updateTask)
 
 export default router;
